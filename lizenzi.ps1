5ce47c5a0aec62e9c456ab7cc9d1db84f862d9b5
wolbach <markus.hotz@hotmail.de>
2022-07-07 16:24:43 +0200

    Update lizenzi.ps1

--- lizenzi.ps1
+++ lizenzi.ps1
@@ -2,35 +2,46 @@
 Skript für zusammenführung von allen Lizenzverwaltungsskripten
 #>
 
-
 if (-not(Get-Module ActiveDirectory))
 { Import-Module ActiveDirectory }
 
-Connect-MsolService
+Connect-MsolService 
 
 $Gruppe = $null
 $Members = $null
-$Uebersicht = $null
+$UPN = $null
 
 $Gruppe = Read-Host "Bitte Kursnamen angeben"
 $Members = Get-ADGroupMember $Gruppe -Server "training.lug-ag.de"
 
 foreach ($Member in $Members){
+
 $Membi = Get-MsolUser -UserPrincipalName ($Member.samaccountname + "@training.lug-ag.de")
-#$Uebersicht = Get-MsolUserLicense -UserPrincipalName $Membi
 
 Write-Host $Membi.DisplayName   ($Membi.licenses).AccountSkuId 
+$UPN += $Membi.DisplayName
 }
 
 $do = Read-Host "
+
 Was soll getan werden?
+
 1 = Standard auf Basic-Lizenz
-2 = ...
+2 = Lizenzen wegnehmen
+(Nichts angeben: Abbrechen)
 "
+
 if ($do -eq 1) {
     Standard-to-Basic -Gruppe $Gruppe -Members $Members
+} elseif ($do -eq 2) {
+    Wegnehmen
+}elseif ($do -eq 3) {
+    <# Action when this condition is true #>
+}elseif ($do -eq 4) {
+    <#condition#>
 }
 
+# Funktionen
 function Standard-to-Basic {
     param (
         $Members,
@@ -54,10 +65,8 @@ Foreach($Member in $Members){
 
     echo "warten auf Office 365 Sync in min (Kann bis zu 35min Dauern)"
 
-    $UPN = $Member.samaccountname + "@training.lug-ag.de"
-
     $ErrorActionPreference = "SilentlyContinue"
-    while ($a -eq $null) {
+    while ($null -eq $a) {
         Sleep 60
         $a = Get-MsolUser -UserPrincipalName $UPN
         [int]$Zahl = $Zahl + 1
@@ -76,3 +85,78 @@ Foreach($Member in $Members){
     Set-MsolUserLicense -UserPrincipalName $UPN -AddLicenses "reseller-account:O365_BUSINESS_ESSENTIALS"
 }
 }
+
+function Wegnehmen {
+
+$a = $null
+$zahl = $null
+
+echo "warten auf Office 365 Sync in min (Kann bis zu 35min Dauern)"
+Foreach($Member in $Members){
+
+
+$UPN = $Member.samaccountname + "@training.lug-ag.de"
+
+$ErrorActionPreference = "SilentlyContinue"
+while ($null -eq $a) {
+      $a = Get-MsolUser -UserPrincipalName $UPN
+      [int]$Zahl = $Zahl + 1
+      Sleep 60
+      }
+
+
+#$LO = New-MsolLicenseOptions -AccountSkuId "reseller-account:O365_BUSINESS_ESSENTIALS"
+#Set-MsolUser -UserPrincipalName $UPN -UsageLocation DE
+#Set-MsolUserLicense -UserPrincipalName $UPN -AddLicenses "reseller-account:O365_BUSINESS_ESSENTIALS" -LicenseOptions $LO
+
+
+### Lizenzen verwalten
+Set-MsolUser -UserPrincipalName $UPN -UsageLocation DE
+Set-MsolUserLicense -UserPrincipalName $UPN -RemoveLicenses "reseller-account:O365_BUSINESS_ESSENTIALS"
+Sleep -Seconds 3
+Set-MsolUserLicense -UserPrincipalName $UPN -RemoveLicenses "reseller-account:O365_BUSINESS_PREMIUM"
+Sleep -Seconds 3
+#Set-MsolUserLicense -UserPrincipalName $UPN -AddLicenses "reseller-account:TEAMS_COMMERCIAL_TRIAL"
+
+Write-Host "Benutzer $UPN bearbeitet"
+
+
+    echo "warten auf Office 365 Sync in min (Kann bis zu 35min Dauern)"
+    Foreach($Member in $Members){
+    
+    
+    $UPN = $Member.samaccountname + "@training.lug-ag.de"
+    
+    $ErrorActionPreference = "SilentlyContinue"
+    while ($null -eq $a) {
+          $a = Get-MsolUser -UserPrincipalName $UPN
+          [int]$Zahl = $Zahl + 1
+          Sleep 60
+          }
+    
+    
+    #$LO = New-MsolLicenseOptions -AccountSkuId "reseller-account:O365_BUSINESS_ESSENTIALS"
+    #Set-MsolUser -UserPrincipalName $UPN -UsageLocation DE
+    #Set-MsolUserLicense -UserPrincipalName $UPN -AddLicenses "reseller-account:O365_BUSINESS_ESSENTIALS" -LicenseOptions $LO
+    
+    
+    ### Lizenzen verwalten
+    Set-MsolUser -UserPrincipalName $UPN -UsageLocation DE
+    Set-MsolUserLicense -UserPrincipalName $UPN -RemoveLicenses "reseller-account:O365_BUSINESS_ESSENTIALS"
+    Sleep -Seconds 3
+    Set-MsolUserLicense -UserPrincipalName $UPN -RemoveLicenses "reseller-account:O365_BUSINESS_PREMIUM"
+    Sleep -Seconds 3
+    #Set-MsolUserLicense -UserPrincipalName $UPN -AddLicenses "reseller-account:TEAMS_COMMERCIAL_TRIAL"
+    
+    Write-Host "Benutzer $Membi bearbeitet"    
+
+}
+}
+}
+
+<#function  {
+    param (
+        OptionalParameters
+    )
+    
+}#>
