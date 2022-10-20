732ef9d9bd7b8b01199665f90851a299afbb8b23
wolbach <markus.hotz@hotmail.de>
2022-10-20 09:49:21 +0200

    Academy Benutzeranlegen V1!!!!1!
    Finalee

--- Training O365 only Benutzeranlegen.ps1
+++ Training O365 only Benutzeranlegen.ps1
@@ -1,9 +1,22 @@
-$user = Import-Csv 
+$dateipfad = "C:\Skripte\"
+$user = Import-Csv -Delimiter ";" -LiteralPath "$dateipfad\user.csv"
+$Group = $null
 
-foreach ($usi in $user) {
+Connect-MsolService
+Connect-MicrosoftTeams
 
-    New-MsolUser 
+$Group = Read-Host "Gruppenname eingeben:"
+
+foreach ($usi in $user){
+$UPN = ($usi.vorname+"."+$usi.name)+"@training.lug-ag.de"
+$sam = $usi.vorname + "." + $usi.name
 
 $pwgen= -join ( (33..39) + (49..57) + (65..90) + (97..107) + (109..122) | Get-Random -Count 10 | Foreach-Object {[char]$_})
-$pw = ConvertTo-SecureString -AsPlainText $pwgen -Force
+$pw = ConvertTo-SecureString -AsPlainText $pwgen -Force 
+
+New-MsolUser -UserPrincipalName $UPN -FirstName $usi.vorname -LastName $usi.name -DisplayName ($usi.vorname+"."+$usi.name) -Password $pw -LicenseAssignment "reseller-account:O365_BUSINESS_PREMIUM" -UsageLocation "DE"
+Get-Team | where DisplayName -eq $Group |Add-TeamUser -User $UPN
+
+"$UPN;$pwgen" >> "$dateipfad\Userlists\$Group.csv"
+
 }
