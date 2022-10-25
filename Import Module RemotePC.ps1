e2d3fd4eb1418b99758a7d351ee225f30beb2a96
wolbach <markus.hotz@hotmail.de>
2022-10-25 09:55:56 +0200

    Update Import Module RemotePC.ps1

--- Import Module RemotePC.ps1
+++ Import Module RemotePC.ps1
@@ -7,5 +7,6 @@
 [securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
 [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
 
-$S = New-PSSession -ComputerName lgka-dpm004 -Credential $credObject -Authentication Negotiate
-Import-Module -PSsession $S -Name # Hier Modulname
+$S = New-PSSession -ComputerName lgka-dc006 -Credential $credObject -Authentication Negotiate
+Import-Module -PSsession $S -Name ActiveDirectory | Install-Module
+#Hier Modulnam
