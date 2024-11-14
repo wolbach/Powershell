8c8475936bb9b2fa3e41056afca7639d92343458
wolbach <83709532+wolbach@users.noreply.github.com>
2024-11-14 13:48:04 +0100

    Add files via upload

--- SQL Instance Cleanup.ps1
+++ SQL Instance Cleanup.ps1
@@ -1,13 +1,15 @@
-﻿$kürzel = "PM"
+﻿$kürzel = "Test2"
 
 $UpdateServ = Get-Service | where Name -eq ('IN-FORM Plattform Update$SQL4TEST_'+$kürzel)
 $PlattformServ = Get-Service | where Name -eq ('IN-FORM Plattform$SQL4TEST_'+$kürzel)
 $DBPath = Get-Item -Path ('D:\SQL_DBs\MSSQL15.SQL4TEST_'+$kürzel) -ErrorAction SilentlyContinue
 $INFORMPath = Get-Item -Path ('D:\SQL4TEST\INFORM_SQL4TEST\'+$kürzel+'_INFORM') -ErrorAction SilentlyContinue
+$dAdminCred = Get-Credential -UserName "in-klr\administrator" -Message "Enter Admin-PW"
 
+Start-Transcript -Path "C:\temp\$kürzel Cleanup.txt"
 #Delete DB Folder if it exists
-if($DBPath){
-    Remove-Item $DBPath -Recurse
+if(Test-Path -LiteralPath $DBPath){
+    Remove-Item $DBPath -Recurse #-Credential $dAdminCred
 }
 
 # Temporary check if local system is configured in service
@@ -15,8 +17,9 @@ $AllServiceStartNames = Get-CimInstance -ClassName CIM_Service | select Name, St
 $selectedService = $AllServiceStartNames | where Name -eq $PlattformServ.Name
 
 if($selectedService.StartName -ne "LocalSystem"){
-    Write-Host -ForegroundColor Red "Neustart könnte nötig sein, bitte nach Abschluss auf Dienststatus achten"
-    sleep 5
+    do {
+        $confirmation = Read-Host "Please set $selectedService Startup Account to LocalSystem. Then enter Y"
+    } while ($confirmation -ne "Y")
 }
 
 # Stop Services when running and deactivate them
@@ -24,6 +27,7 @@ while($UpdateServ.Status -eq "Running"){
     Write-Host "Stoppe Update-Dienst..."
     Stop-Service -Name $UpdateServ.Name -NoWait
     sleep 10
+    $UpdateServ.Refresh()
 }
 Write-Host "Deaktiviere Autostart des Update-Dienstes..."
 Set-Service -Name $UpdateServ.Name  -StartupType Disabled -ComputerName "SQL4TEST" 
@@ -32,6 +36,7 @@ while($PlattformServ.Status -eq "Running"){
     Write-Host "Stoppe Plattform-Dienst..."
     Stop-Service -Name $PlattformServ.Name -NoWait
     sleep 10
+    $PlattformServ.Refresh()
 }
 Write-Host "Deaktiviere Autostart des Plattformdienstes..."
 Set-Service -Name $PlattformServ.Name -StartupType Disabled -ComputerName "SQL4TEST" 
@@ -39,5 +44,13 @@ Set-Service -Name $PlattformServ.Name -StartupType Disabled -ComputerName "SQL4T
 #Delete INFORM Path if it exists
 Write-Host "Lösche "+$kürzel+"_INFORM"
 if($INFORMPath){
-    Remove-Item $INFORMPath -Recurse 
+    Remove-Item $INFORMPath -Recurse -Force
 }
+
+#Get and delete all FW Rules
+Get-NetFirewallRule -All | where DisplayName -eq "Freigabe für IN-FORM SQL4TEST_$kürzel" | Remove-NetFirewallRule
+Write-Host "SQL Server Firewall Rule deleted"
+Get-NetFirewallRule -All | where DisplayName -eq "Freigabe für WIP.exe SQL4TEST_$kürzel" | Remove-NetFirewallRule
+Write-Host "WIP Firewall Rule deleted"
+
+Stop-Transcript
