97a25bb59e5ad6bb3492e450efc09c5fc5d104f5
wolbach <83709532+wolbach@users.noreply.github.com>
2025-08-18 17:03:47 +0200

    Web Automation Test

---
+++ Selenium/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTest;
+
+public class TestScript
+{
+    private static string GetEdgeLocation()
+        {
+            return Environment.GetEnvironmentVariable("EDGE_BIN");
+        }
+    public static void Main()
+    {
+
+        //EdgeOptions browseSettings = new EdgeOptions();
+        
+
+        FirefoxOptions browseSettings = new FirefoxOptions();
+        Proxy proxy_srv = new Proxy();
+
+        proxy_srv.Kind = ProxyKind.Manual;
+        proxy_srv.IsAutoDetect = false;
+        proxy_srv.SslProxy = "proxy-srv.in-klr.com:8081";
+
+        browseSettings.Proxy = proxy_srv;
+        browseSettings.AddArgument("ignore-certificate-errors");
+        browseSettings.AcceptInsecureCertificates = true;
+        //browseSettings.BinaryLocation = TestScript.GetEdgeLocation();
+
+        //IWebDriver driv = new EdgeDriver(browseSettings);
+        var driv = new FirefoxDriver(browseSettings);
+
+        driv.Navigate().GoToUrl("https://10.20.32.247/cgi-bin/login");
+
+        var username_box = driv.FindElement(By.ClassName("login-email-input__input"));
+
+        username_box.SendKeys("admin");
+
+        driv.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        driv.Quit();
+    }
+}
--- Swyx - Add Intrusion Number.ps1
+++ Swyx - Add Intrusion Number.ps1
@@ -1,4 +1,6 @@
-$recipientList = "MAH"
+$recipientList = "RNE, CKT, GOP, SKN, VPA"
+
+$recipientList = $recipientList.Split(",").Trim()
 $subjectList = "SGU"
 
 Connect-IpPbx
@@ -6,27 +8,29 @@ Connect-IpPbx
 
 $CollectionEntry = New-Object "SWCOnfigDataClientLib.Proxies.Users.UserIntrusionNumberEntry"
 $subjectNumbers = @()
-foreach ($recipient in $recipientList) {
-        $recipientUser = Get-IpPbxUser -UserName $recipient
-        $currSubjects = $recipientUser.UserIntrusionNumberEntryCollection
-        foreach ($subject in $subjectList) {
+
+foreach ($subject in $subjectList) {
                 $su = Get-IpPbxUser -UserName $subject
                 $suNumbers = $su.InternalNumberEntryCollection[0].InternalNumberID
                 $subjectNumbers += $suNumbers
-                
             }
+
+foreach ($recipient in $recipientList) {
+        $recipientUser = Get-IpPbxUser -UserName $recipient
+        $currSubjects = $recipientUser.UserIntrusionNumberEntryCollection
+        
         
         for ($i = 0; $i -lt $subjectNumbers.Count; $i++) {
             if ($subjectNumbers[$i] -notin $currSubjects) {
                 
-                $subjectInternalNumber = Get-IpPbxInternalNumber -InternalNumberId $subjectNumbers[$i]
+                $subjectInternalNumber = Get-IpPbxInternalNumber -InternalNumberId $subjectNumbers[$i] | select Number
                 $recipientInternalNumber = $recipientUser.InternalNumberEntryCollection[0].Number
 
                 $CollectionEntry.UserID = $recipientUser.UserID
                 $CollectionEntry.InternalNumberID = $subjectNumbers[$i]
                 $recipientUser.UserIntrusionNumberEntryCollection.Add($CollectionEntry)
                 Update-IpPbxUser -UserEntry $recipientUser
-                Write-Host "added "($subjectNumbers[$i])" to "+$recipientUser.Name
+                Write-Host "added "$subjectInternalNumber" to "$recipientInternalNumber
             }else {
                 continue
             }
